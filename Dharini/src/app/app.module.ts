ea8b4f5d75213b69f96b362af87f39a00dcbe6a4
DhariniBN <144118862+DhariniBN@users.noreply.github.com>
2023-10-06 15:13:45 +0530

    Add files via upload

--- Dharini/src/app/app.module.ts
+++ Dharini/src/app/app.module.ts
@@ -3,28 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TeacherComponent } from './Components/teacher/teacher.component';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-
-
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TeacherComponent,
-    //AddQuestionComponent,
-    //DeletequeComponent,
-  
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    AppRoutingModule,
-    HttpClientModule,
-    BrowserAnimationsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
